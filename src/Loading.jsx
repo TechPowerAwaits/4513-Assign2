1d87e239b6c3c4ad694acf6cacfb8191f7f2383e
Richard Johnston <techpowerawaits@outlook.com>
2025-03-15 13:03:24 -0600

    Add Loading component (#1)
    * Add initial Loading component
    
    * Improve Loading image
    
    * Make Loading animation less nausiating
    
    * Improve Loading component

---
+++ src/Loading.jsx
@@ -0,0 +1,26 @@
+/*
+ * Purpose: Provides a component for providing a nice animation while something
+ * is loading.
+ */
+
+import loadingImg from "./assets/Loading.png";
+
+/*
+ * Purpose: Provides a Loading animation.
+ */
+function Loading() {
+  return (
+    <figure className="space-y-8.5">
+      <img
+        src={loadingImg}
+        alt="Loading..."
+        className="motion-safe:animate-[spin_1.5s_infinite] mx-auto"
+      />
+      <figcaption className="text-2xl font-bold text-center">
+        Loading...
+      </figcaption>
+    </figure>
+  );
+}
+
+export default Loading;
