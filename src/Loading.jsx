4ed2ca51e717cfe46800dd341c0c25d2b0423661
Richard Johnston <techpowerawaits@outlook.com>
2025-03-15 12:38:19 -0600

    Improve Loading component

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -1,15 +1,25 @@
+/*
+ * Purpose: Provides a component for providing a nice animation while something
+ * is loading.
+ */
+
 import loadingImg from "./assets/Loading.png";
 
+/*
+ * Purpose: Provides a Loading animation.
+ */
 function Loading() {
   return (
-    <div>
-      <h2 className="text-2xl font-bold text-center">Loading...</h2>
+    <figure className="space-y-8.5">
       <img
         src={loadingImg}
         alt="Loading..."
-        className="motion-safe:animate-[spin_1.5s_infinite] mt-8.5 ml-auto mr-auto"
+        className="motion-safe:animate-[spin_1.5s_infinite] mx-auto"
       />
-    </div>
+      <figcaption className="text-2xl font-bold text-center">
+        Loading...
+      </figcaption>
+    </figure>
   );
 }
 
