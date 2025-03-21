89befa925cad7fc308e0b7436877ff2a01f1045d
Richard Johnston <techpowerawaits@outlook.com>
2025-03-21 15:43:25 -0600

    Add more styling to the Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,21 +3,33 @@
 import { useState } from "react";
 
 function Login(props) {
-  const [msg, setMsg] = useState("");
+  const defaultMsg =
+    "Registration is not yet supported. Please login as a guest.";
+  const [msg, setMsg] = useState(defaultMsg);
   const [isGuest, setGuest] = useState(false);
 
   return (
-    <article className="bg-[url(assets/brooklyn-MO5qO9xpZhA-unsplash.jpg)] bg-no-repeat bg-cover">
-      <h1>Welcome to Art Browser!</h1>
-      <section>{msg}</section>
-      <form onSubmit={handleSubmit}>
-        <fieldset className="grid grid-cols-2">
-          <legend className="col-span-full">Login</legend>
+    <article className="bg-[url(assets/brooklyn-MO5qO9xpZhA-unsplash.jpg)] bg-no-repeat bg-cover space-y-3">
+      <h1 className="bg-blue-700 text-yellow-400 text-2xl font-bold text-center">
+        Welcome to Art Browser!
+      </h1>
+      <section className="bg-blue-700 text-yellow-400 text-center">
+        <h2 className="text-xl font-bold">Status:</h2>
+        {msg}
+      </section>
+      <form
+        onSubmit={handleSubmit}
+        className="text-yellow-400 bg-green-700 py-1.5"
+      >
+        <fieldset className="grid grid-cols-2 space-y-3">
+          <legend className="col-span-full text-center font-bold text-xl">
+            Login
+          </legend>
           <label htmlFor="username">Username:</label>
           <input
             autoFocus
             autoComplete="username"
-            className="bg-amber-200 border-black border-2 disabled:bg-gray-400"
+            className="bg-yellow-200 border-black border-2 disabled:bg-gray-400"
             disabled={isGuest}
             type="text"
             name="username"
@@ -25,32 +37,40 @@ function Login(props) {
           <label htmlFor="password">Password:</label>
           <input
             autoComplete="current-password"
-            className="bg-amber-200 border-black border-2 disabled:bg-gray-400"
+            className="bg-yellow-200 border-black border-2 disabled:bg-gray-400"
             disabled={isGuest}
             type="password"
             name="password"
           />
-          <input
-            checked={isGuest}
-            value="guest"
-            type="checkbox"
-            name="isGuest"
-            onChange={toggleGuest}
-          ></input>
-          <label htmlFor="isGuest">Login as Guest</label>
-          <button className="col-span-full bg-blue-700 hover:bg-blue-400">
+          <fieldset>
+            <input
+              checked={isGuest}
+              value="guest"
+              type="checkbox"
+              name="isGuest"
+              onChange={toggleGuest}
+            ></input>
+            <label htmlFor="isGuest">Login as Guest</label>
+          </fieldset>
+          <button
+            className="col-span-full bg-blue-700 hover:bg-blue-400"
+            type="submit"
+          >
             Login
           </button>
         </fieldset>
       </form>
-      Photo by{" "}
-      <a href="https://unsplash.com/@brooklyngrace?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-        brooklyn
-      </a>{" "}
-      on{" "}
-      <a href="https://unsplash.com/photos/people-in-the-street-painting-MO5qO9xpZhA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-        Unsplash
-      </a>
+      <section className="bg-blue-700 text-yellow-400 text-center">
+        <h2 className="text-xl font-bold">Image Credits:</h2>
+        Photo by{" "}
+        <a href="https://unsplash.com/@brooklyngrace?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          brooklyn
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/people-in-the-street-painting-MO5qO9xpZhA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </section>
     </article>
   );
 
@@ -60,7 +80,7 @@ function Login(props) {
     if (isGuest) {
       props.handler(true);
     } else {
-      setMsg("invalid");
+      setMsg("No account is associated with the given username and password.");
     }
   }
 
