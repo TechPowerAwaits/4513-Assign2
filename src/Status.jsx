e11e7ec32bd18081f0651d9370cef30d6d7bff11
Richard Johnston <techpowerawaits@outlook.com>
2025-03-23 16:52:29 -0600

    Use rounded corners with Status

--- src/Status.jsx
+++ src/Status.jsx
@@ -3,7 +3,7 @@ import H from "./H";
 function Status({ msg, isErr = false, className: passedClasses }) {
   return (
     <section
-      className={`${isErr ? "bg-red-700" : "bg-blue-700"} text-yellow-400 text-center ${passedClasses}`}
+      className={`${isErr ? "bg-red-700" : "bg-blue-700"} text-yellow-400 text-center px-3 py-1 rounded-3xl ${passedClasses}`}
     >
       <H.L4>Status:</H.L4>
       {msg}
