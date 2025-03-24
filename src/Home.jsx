fa75a14ee7351aad3f45bebcb1a356999915d3b1
Richard Johnston <techpowerawaits@outlook.com>
2025-03-23 19:21:08 -0600

    Improve arrangement of buttons on Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -31,20 +31,26 @@ function ChoiceButtons({ className: passedClasses }) {
   return (
     <section className={`p-1.5 ${passedClasses}`}>
       <H.L3>What would you like to do?</H.L3>
-      <button
-        type="button"
-        className="cursor-pointer bg-tyrian-purple hover:bg-murrey text-ut-orange mx-auto py-3 px-4 rounded-full"
-      >
-        Create An Account
-      </button>
-      <Link to="/login">
-        <button
-          type="button"
-          className="cursor-pointer bg-midnight-green hover:bg-caribbean-current text-ut-orange mx-auto py-3 px-4 rounded-full"
-        >
-          Sign In
-        </button>
-      </Link>
+      <menu className="flex justify-center gap-4 sm:flex-row flex-col">
+        <li className="mx-auto sm:mx-0">
+          <button
+            type="button"
+            className="hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-tyrian-purple hover:bg-murrey text-ut-orange py-3 px-4 rounded-full"
+          >
+            Create An Account
+          </button>
+        </li>
+        <li className="mx-auto sm:mx-0">
+          <Link to="/login">
+            <button
+              type="button"
+              className="hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-midnight-green hover:bg-caribbean-current text-ut-orange py-3 px-4 rounded-full"
+            >
+              Sign In
+            </button>
+          </Link>
+        </li>
+      </menu>
     </section>
   );
 }
