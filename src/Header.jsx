727f2f4a9d31ed0ee7b1a9920f200b8811d56853
Richard Johnston <techpowerawaits@outlook.com>
2025-03-23 20:52:38 -0600

    Use new color scheme in Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ import H from "./H";
 
 function Header() {
   return (
-    <header className="flex justify-between bg-blue-700 text-yellow-400">
+    <header className="flex justify-between bg-tyrian-purple text-ut-orange">
       <img alt="Logo" />
       <H.L1>Art Browser</H.L1>
       <nav></nav>
