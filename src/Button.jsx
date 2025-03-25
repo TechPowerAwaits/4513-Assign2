62389405e5b266dfdeba109820e32116914d1584
Richard Johnston <techpowerawaits@outlook.com>
2025-03-25 09:57:24 -0600

    Fix bug with passed className to Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -10,7 +10,7 @@ class Button {
   static Primary({ className: passedClasses, children, ...props }) {
     return (
       <button
-        className={`hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-tyrian-purple hover:bg-murrey text-ut-orange py-3 px-4 rounded-full ${passedClasses}`}
+        className={`hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-tyrian-purple hover:bg-murrey text-ut-orange py-3 px-4 rounded-full ${passedClasses ? passedClasses : ""}`}
         {...props}
       >
         {children}
@@ -21,7 +21,7 @@ class Button {
   static Secondary({ className: passedClasses, children, ...props }) {
     return (
       <button
-        className={`hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-midnight-green hover:bg-caribbean-current text-ut-orange py-3 px-4 rounded-full ${passedClasses}`}
+        className={`hover:scale-105 motion-reduce:hover:scale-100 border-2 border-transparent active:border-black hover:border-black cursor-pointer bg-midnight-green hover:bg-caribbean-current text-ut-orange py-3 px-4 rounded-full ${passedClasses ? passedClasses : ""}`}
         {...props}
       >
         {children}
