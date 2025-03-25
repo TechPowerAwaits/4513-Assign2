acfdf51bdc24b227b9641a559e007d977225438a
Richard Johnston <techpowerawaits@outlook.com>
2025-03-25 09:41:37 -0600

    Add Logo to Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,10 @@
 import H from "./H";
+import Logo from "./assets/Logo.png";
 
 function Header() {
   return (
     <header className="flex justify-between bg-tyrian-purple text-ut-orange">
-      <img alt="Logo" />
+      <img src={Logo} alt="Logo" />
       <H.L1>Art Browser</H.L1>
       <nav></nav>
     </header>
