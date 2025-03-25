9e395ddb19f6fb9d1510e944473c34bc975e7c7f
Richard Johnston <techpowerawaits@outlook.com>
2025-03-25 16:05:39 -0600

    Indicate in Header when we are running Dev build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,13 @@ import H from "./H";
 import Logo from "../assets/Logo.png";
 
 function Header() {
+  let headerTitle = "Art Browser";
+  headerTitle += import.meta.env.DEV ? " DEV" : "";
+
   return (
     <header className="flex justify-between bg-tyrian-purple text-ut-orange">
       <img src={Logo} alt="Logo" />
-      <H.L1>Art Browser</H.L1>
+      <H.L1>{headerTitle}</H.L1>
       <nav></nav>
     </header>
   );
