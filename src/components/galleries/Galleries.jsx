59e909428bf94bc896e513dac3259816a482fb42
Richard Johnston <techpowerawaits@outlook.com>
2025-03-27 02:21:08 -0600

    Remove useGalleriesData from Galleries

--- src/components/galleries/Galleries.jsx
+++ src/components/galleries/Galleries.jsx
@@ -1,17 +1,7 @@
 import GalleryList from "./GalleryList";
 import H from "../H";
-import useGalleriesData from "../../hooks/useGalleriesData";
 
 function Galleries() {
-  const galleryData = useGalleriesData();
-  console.table(galleryData);
-
-  // When the hook is first used, it uses the initial value instead of the value in the localStore.
-  /*if (galleryData.length == 0) {
-    setGalleryData(galleryTestData);
-  }*/
-  console.table(galleryData);
-
   return (
     <article>
       <header className="bg-tyrian-purple text-ut-orange border-y border-mimi-pink">
