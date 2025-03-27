a9d373dfe3715854a7cd75b9c4764a6ffcbe72c9
Richard Johnston <techpowerawaits@outlook.com>
2025-03-27 10:18:52 -0600

    Stop fetching data from Galleries

--- src/components/galleries/Galleries.jsx
+++ src/components/galleries/Galleries.jsx
@@ -1,27 +1,7 @@
 import GalleryList from "./GalleryList";
 import H from "../H";
-import { useEffect, useState } from "react";
-import { fetchCachedObjFromJSON } from "../../fetchHandler";
 
 function Galleries() {
-  const [galleryData, setGalleryData] = useState(null);
-  useEffect(() => {
-    const url = "https://art-api-kafs.onrender.com/api/galleries";
-    const key = "galleries";
-
-    if (!galleryData) {
-      try {
-        fetchCachedObjFromJSON(key, url, setGalleryData, (error) =>
-          console.error(error.message)
-        );
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-  }, [galleryData]);
-
-  console.table(galleryData);
-
   return (
     <article>
       <header className="bg-tyrian-purple text-ut-orange border-y border-mimi-pink">
