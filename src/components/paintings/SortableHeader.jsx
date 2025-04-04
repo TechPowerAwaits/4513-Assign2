d4087bde73185443f09c85e9ef2b08e030c964a6
Richard Johnston <techpowerawaits@outlook.com>
2025-04-04 12:31:34 -0600

    Add documentation to SortableHeader

--- src/components/paintings/SortableHeader.jsx
+++ src/components/paintings/SortableHeader.jsx
@@ -1,8 +1,32 @@
+/*
+ * Purpose: Provides a button-based component for managing the state of sorting
+ * a list.
+ */
+
 import { use, useMemo, useState } from "react";
 import { CurrentSortContext } from "../../contexts/Sort";
 
+/*
+ * Purpose: Provides named values for the state of an arrow.
+ */
 const ARROW_ENUM = { UP: 0, DOWN: 1 };
 
+/*
+ * Purpose: Provides a togglable button for sorting by a given field.
+ *
+ * Purpose: The component requires the CurrentSortContext to be an array
+ * containing the following:
+ *
+ * Idx:    Can Contain:
+ * 0       Any value, but should be a string representing a column to sort by.
+ * 1       A function to set the value within index 0.
+ *
+ * The setAscending prop will be passed true or false depending on the internal
+ * state of the header when clicked on.
+ *
+ * The component contains some default styling. All props and classes are
+ * passed to the underlying element.
+ */
 function SortableHeader({
   sortId,
   text,
@@ -13,6 +37,10 @@ function SortableHeader({
   const [sortCol, setSortCol] = use(CurrentSortContext);
   const [arrowDir, setArrowDir] = useState(ARROW_ENUM.DOWN);
 
+  /*
+   * Purpose: Removes the arrow indicating sorting direction from the header if
+   * sorting isn't occurring as a result of the header.
+   */
   useMemo(() => {
     if (sortCol != sortId) {
       setArrowDir(null);
