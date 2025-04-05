85f7d9bdb32ae497c1af8daeb88fc07cb8e3c462
Richard Johnston <techpowerawaits@outlook.com>
2025-04-05 17:01:16 -0600

    Correct styling and functionality of ErrorHandler

--- src/components/ErrorHandler.jsx
+++ src/components/ErrorHandler.jsx
@@ -7,9 +7,9 @@ import H from "./H";
 
 function ErrorHandler({ errToggle }) {
   return (
-    <article>
+    <article className="flex flex-col align-middle">
       <H.L2>An Error has Occurred</H.L2>
-      <Button.Primary className="mx-auto" onclick={() => errToggle()}>
+      <Button.Primary className="mx-auto" onClick={() => errToggle()}>
         Retry
       </Button.Primary>
     </article>
