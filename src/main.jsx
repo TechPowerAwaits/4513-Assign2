d6008907bfa5bebd5af1bd2b77d4782a829abbc6
Richard Johnston <techpowerawaits@outlook.com>
2025-04-05 12:52:05 -0600

    Add NiceModal.Provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import Modal from "react-modal";
+import NiceModal from "@ebay/nice-modal-react";
 
 Modal.setAppElement("#root");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <NiceModal.Provider>
+        <App />
+      </NiceModal.Provider>
     </BrowserRouter>
   </StrictMode>
 );
