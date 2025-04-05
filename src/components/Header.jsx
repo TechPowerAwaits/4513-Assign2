66c05275066490023f81ed171743abca37db41d8
Richard Johnston <techpowerawaits@outlook.com>
2025-04-04 18:35:51 -0600

    Add intial sign out and about buttons to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,12 @@
 
 import H from "./H";
 import Logo from "../assets/Logo.png";
+import { use } from "react";
+import { AccountContext } from "../contexts/Account";
+import Button from "./Button";
 
 function Header() {
+  const { account, setAccount } = use(AccountContext);
   let headerTitle = "Art Browser";
   headerTitle += import.meta.env.DEV ? " DEV" : "";
 
@@ -13,7 +17,14 @@ function Header() {
     <header className="flex justify-between bg-tyrian-purple text-ut-orange">
       <img src={Logo} alt="Logo" />
       <H.L1>{headerTitle}</H.L1>
-      <nav></nav>
+      <nav className="flex">
+        {account && (
+          <Button.Secondary onClick={() => setAccount(null)}>
+            Sign Out
+          </Button.Secondary>
+        )}
+        <Button.Primary>About</Button.Primary>
+      </nav>
     </header>
   );
 }
