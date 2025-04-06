f7cf99b0e7409f46264e96e992e99efc70d8a153
Richard Johnston <techpowerawaits@outlook.com>
2025-04-05 22:45:51 -0600

    Add exit button to PaintingFilterMenu

--- src/components/paintings/PaintingFilterMenu.jsx
+++ src/components/paintings/PaintingFilterMenu.jsx
@@ -1,10 +1,14 @@
 import PaintingFilter from "./PaintingFilter";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
+import Button from "../Button";
 
 function PaintingFilterMenu({ isOpen, onClose, ...props }) {
   return (
     <Drawer size={400} open={isOpen} onClose={onClose} direction="right">
+      <Button.Primary className="absolute" onClick={onClose}>
+        X
+      </Button.Primary>
       <PaintingFilter
         className="h-full"
         onFilter={onClose}
