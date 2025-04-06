38d5cbe8454e621ff1853a775f817aea48378334
Richard Johnston <techpowerawaits@outlook.com>
2025-04-05 22:32:11 -0600

    Utilize react-modern-drawer in PaintingFilterMenu

--- src/components/paintings/PaintingFilterMenu.jsx
+++ src/components/paintings/PaintingFilterMenu.jsx
@@ -1,18 +1,17 @@
 import PaintingFilter from "./PaintingFilter";
-import { slide as SlideMenu } from "react-burger-menu";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 function PaintingFilterMenu({ isOpen, onClose, ...props }) {
   return (
-    <SlideMenu
-      itemListElement="div"
-      width={400}
-      isOpen={isOpen}
-      onClose={onClose}
-      customBurgerIcon={false}
-      customCrossIcon={false}
-    >
-      <PaintingFilter onFilter={onClose} onReset={onClose} {...props} />
-    </SlideMenu>
+    <Drawer size={400} open={isOpen} onClose={onClose} direction="right">
+      <PaintingFilter
+        className="h-full"
+        onFilter={onClose}
+        onReset={onClose}
+        {...props}
+      />
+    </Drawer>
   );
 }
 
