473abaab9cb4a1dd1655f2f1d5049ff9cb546db7
Richard Johnston <techpowerawaits@outlook.com>
2025-04-06 13:58:09 -0600

    Add registration of PaintingModal

--- src/modals.js
+++ src/modals.js
@@ -4,9 +4,11 @@
 
 import NiceModal from "@ebay/nice-modal-react";
 import AboutModal from "./components/about/AboutModal";
+import PaintingModal from "./components/paintings/modal/PaintingModal";
 
 function initModals() {
   NiceModal.register("about-project", AboutModal);
+  NiceModal.register("painting-modal", PaintingModal);
 }
 
 export default initModals;
