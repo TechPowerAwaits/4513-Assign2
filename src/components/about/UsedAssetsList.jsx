d5cfa1cfa72c8d2278208b3fc5e6cf17e09c21e7
Richard Johnston <techpowerawaits@outlook.com>
2025-04-06 13:01:09 -0600

    Use Hyperlink in UsedAssetsList

--- src/components/about/UsedAssetsList.jsx
+++ src/components/about/UsedAssetsList.jsx
@@ -1,4 +1,5 @@
 import H from "../H";
+import Hyperlink from "../Hyperlink";
 
 function UsedAssetsList() {
   return (
@@ -8,48 +9,31 @@ function UsedAssetsList() {
         <li>
           <span className="font-bold">Loading Image:</span> Original obtained
           from{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://openclipart.org/detail/279443/golden-frame"
-          >
+          <Hyperlink href="https://openclipart.org/detail/279443/golden-frame">
             https://openclipart.org/detail/279443/golden-frame
-          </a>
+          </Hyperlink>
           . It was shrunk by 50% and the transparent portion was replaced with
           white. Text was added instructing the user to wait.
         </li>
         <li>
           <span className="font-bold">Home Page Hero Image:</span> Photo by{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://unsplash.com/@brooklyngrace"
-          >
+          <Hyperlink href="https://unsplash.com/@brooklyngrace">
             brooklyn
-          </a>{" "}
+          </Hyperlink>{" "}
           on{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://unsplash.com/photos/people-in-the-street-painting-MO5qO9xpZhA"
-          >
+          <Hyperlink href="https://unsplash.com/photos/people-in-the-street-painting-MO5qO9xpZhA">
             Unsplash
-          </a>
+          </Hyperlink>
           .
         </li>
         <li>
           <span className="font-bold">Fallback Image:</span> The font used in
           the image (5by7) came from{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://www.dafont.com/5by7.font"
-          >
-            Dafont
-          </a>{" "}
+          <Hyperlink href="https://www.dafont.com/5by7.font">Dafont</Hyperlink>{" "}
           and was made by{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://www.dafont.com/peter-wiegel.d689"
-          >
+          <Hyperlink href="https://www.dafont.com/peter-wiegel.d689">
             Peter Wiegel
-          </a>
+          </Hyperlink>
           .
         </li>
         <li>
@@ -57,12 +41,9 @@ function UsedAssetsList() {
             The artist, genre, and painting images:
           </span>{" "}
           Were provided by{" "}
-          <a
-            className="underline visited:text-spanish-orange hover:text-sandy-brown"
-            href="https://www.randyconnolly.com/"
-          >
+          <Hyperlink href="https://www.randyconnolly.com/">
             Randy Connolly
-          </a>
+          </Hyperlink>
         </li>
       </ul>
     </section>
