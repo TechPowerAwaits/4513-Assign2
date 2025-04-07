12d0d28724fa0d3829c366840e4f1048e4784ef3
Richard Johnston <techpowerawaits@outlook.com>
2025-04-06 18:37:06 -0600

    Allow classes to be passed to Hyperlink

--- src/components/Hyperlink.jsx
+++ src/components/Hyperlink.jsx
@@ -2,10 +2,15 @@
  * Purpose: Provides a styled component for hyperlinks.
  */
 
-function Hyperlink({ href = "#", children, ...props }) {
+function Hyperlink({
+  href = "#",
+  className: passedClasses = "",
+  children,
+  ...props
+}) {
   return (
     <a
-      className="underline text-ut-orange visited:text-spanish-orange hover:text-sandy-brown"
+      className={`underline text-ut-orange visited:text-spanish-orange hover:text-sandy-brown ${passedClasses}`}
       href={href}
       {...props}
     >
