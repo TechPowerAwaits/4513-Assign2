7d7b016ed51c48bdf3c6d9cc36c4960928b0bde4
Richard Johnston <techpowerawaits@outlook.com>
2025-04-06 20:24:53 -0600

    Revert "Initial switch of PaintingList to actual list"
    This reverts commit 929cf213b7003671b1305b45a6a0d90dc8881fdd.

--- src/components/paintings/PaintingListItem.jsx
+++ src/components/paintings/PaintingListItem.jsx
@@ -2,57 +2,39 @@ import NiceModal from "@ebay/nice-modal-react";
 import ImageWithFallback from "../ImageWithFallback";
 
 function PaintingListItem({ painting, permittedCols }) {
-  const clickHandler = () =>
-    NiceModal.show("painting-modal", { data: painting });
   return (
-    <>
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("thumbnail") && (
+    <tr
+      className="cursor-pointer"
+      onClick={() => NiceModal.show("painting-modal", { data: painting })}
+    >
+      {permittedCols.includes("thumbnail") && (
+        <td>
           <ImageWithFallback
-            className="max-w-1/2 max-h-1/2"
+            className="scale-25"
             src={`/paintings/square/${painting.imageFileName}.jpg`}
             alt={painting.title}
           />
-        )}
-      </li>
-      {permittedCols.includes("artist") ? (
+        </td>
+      )}
+      {permittedCols.includes("artist") && (
         <>
-          <li className="cursor-pointer" onClick={clickHandler}>
-            {painting.Artists.firstName}
-          </li>
-          <li className="cursor-pointer" onClick={clickHandler}>
-            {painting.Artists.lastName}
-          </li>
-        </>
-      ) : (
-        <>
-          <li className="cursor-pointer" onClick={clickHandler}></li>
-          <li className="cursor-pointer" onClick={clickHandler}></li>
+          <td>{painting.Artists.firstName}</td>
+          <td>{painting.Artists.lastName}</td>
         </>
       )}
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("title") && <p>{painting.title}</p>}
-      </li>
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("year") && <p>{painting.yearOfWork}</p>}
-      </li>
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("gallery") && (
-          <p>painting.Galleries.galleryName</p>
-        )}
-      </li>
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("medium") && <p>{painting.medium}</p>}
-      </li>
-      <li className="cursor-pointer" onClick={clickHandler}>
-        {permittedCols.includes("dimensions") && (
-          <p>
-            {painting.width} <span className="font-semibold">x</span>{" "}
-            {painting.height}
-          </p>
-        )}
-      </li>
-    </>
+      {permittedCols.includes("title") && <td>{painting.title}</td>}
+      {permittedCols.includes("year") && <td>{painting.yearOfWork}</td>}
+      {permittedCols.includes("gallery") && (
+        <td>{painting.Galleries.galleryName}</td>
+      )}
+      {permittedCols.includes("medium") && <td>{painting.medium}</td>}
+      {permittedCols.includes("dimensions") && (
+        <td>
+          {painting.width} <span className="font-semibold">x</span>{" "}
+          {painting.height}
+        </td>
+      )}
+    </tr>
   );
 }
 
